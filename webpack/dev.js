3fa1282af5824c735aae7cb4d9023d698b1db700
i.romanova <i.romanova@fun-box.ru>
2023-01-10 15:33:01 +0400

    initial commit

---
+++ webpack/dev.js
@@ -0,0 +1,49 @@
+const Html = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: [
+      'core-js/stable/promise',
+      'core-js/stable/symbol',
+      path.resolve(__dirname, '/src/app/app.js')
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              exclude: /node_modules[\\/]core-js[\\/].*/,
+              presets: [
+                ['@babel/preset-env', {
+                  useBuiltIns: 'entry',
+                  corejs: '3.27.1',
+                  modules: 'auto',
+                  targets: { esmodules: true },
+                }],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Html({
+      template: path.resolve(__dirname, '/src/app/index.html')
+    }),
+  ],
+  devServer: {
+    hot: true,
+  },
+};
